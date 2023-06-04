3f027ef6199150ba1415047c86a6b3cd393147f3
thienkhoi2805 <thienkhoi28501@gmail.com>
2023-06-04 20:43:07 +0700

    Trang dang nhap va quan ly nguoi dung

---
+++ QLGV_DTSoft/Controllers/NguoiDungsController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QLGV_DTSoft.Data;
+using QLGV_DTSoft.Helper;
+
+namespace QLGV_DTSoft.Controllers
+{
+    [Authorize]
+    public class NguoiDungsController : Controller
+    {
+        private readonly DtsoftContext _context;
+
+        public NguoiDungsController(DtsoftContext context)
+        {
+            _context = context;
+        }
+
+        // GET: NguoiDungs
+        public async Task<IActionResult> Index()
+        {
+            var dtsoftContext = _context.NguoiDungs.Include(n => n.IdBpNavigation).Include(n => n.IdVtNavigation);
+            return View(await dtsoftContext.ToListAsync());
+        }
+
+        // GET: NguoiDungs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.NguoiDungs == null)
+            {
+                return NotFound();
+            }
+
+            var nguoiDung = await _context.NguoiDungs
+                .Include(n => n.IdBpNavigation)
+                .Include(n => n.IdVtNavigation)
+                .FirstOrDefaultAsync(m => m.IdNd == id);
+            if (nguoiDung == null)
+            {
+                return NotFound();
+            }
+
+            return View(nguoiDung);
+        }
+
+        // GET: NguoiDungs/Create
+        public IActionResult Create()
+        {
+            ViewData["IdBp"] = new SelectList(_context.BoPhans, "IdBp", "Tenbophan");
+            ViewData["IdVt"] = new SelectList(_context.VaiTros, "IdVt", "Tenvaitro");
+            return View();
+        }
+
+        // POST: NguoiDungs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdNd,IdVt,IdBp,Tennguoidung,Matkhau,Hoten,Ngaysinh,Gioitinh,Sodienthoai,Diachi,Email")] NguoiDung nguoiDung)
+        {
+            if (ModelState.IsValid)
+            {
+                nguoiDung.Matkhau = SecretHasher.Hash(nguoiDung.Matkhau);
+                _context.Add(nguoiDung);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdBp"] = new SelectList(_context.BoPhans, "IdBp", "IdBp", nguoiDung.IdBp);
+            ViewData["IdVt"] = new SelectList(_context.VaiTros, "IdVt", "IdVt", nguoiDung.IdVt);
+            return View(nguoiDung);
+        }
+
+        // GET: NguoiDungs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.NguoiDungs == null)
+            {
+                return NotFound();
+            }
+
+            var nguoiDung = await _context.NguoiDungs.FindAsync(id);
+            if (nguoiDung == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdBp"] = new SelectList(_context.BoPhans, "IdBp", "IdBp", nguoiDung.IdBp);
+            ViewData["IdVt"] = new SelectList(_context.VaiTros, "IdVt", "IdVt", nguoiDung.IdVt);
+            return View(nguoiDung);
+        }
+
+        // POST: NguoiDungs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdNd,IdVt,IdBp,Tennguoidung,Matkhau,Hoten,Ngaysinh,Gioitinh,Sodienthoai,Diachi,Email")] NguoiDung nguoiDung)
+        {
+            if (id != nguoiDung.IdNd)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nguoiDung);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NguoiDungExists(nguoiDung.IdNd))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdBp"] = new SelectList(_context.BoPhans, "IdBp", "IdBp", nguoiDung.IdBp);
+            ViewData["IdVt"] = new SelectList(_context.VaiTros, "IdVt", "IdVt", nguoiDung.IdVt);
+            return View(nguoiDung);
+        }
+
+        // GET: NguoiDungs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.NguoiDungs == null)
+            {
+                return NotFound();
+            }
+
+            var nguoiDung = await _context.NguoiDungs
+                .Include(n => n.IdBpNavigation)
+                .Include(n => n.IdVtNavigation)
+                .FirstOrDefaultAsync(m => m.IdNd == id);
+            if (nguoiDung == null)
+            {
+                return NotFound();
+            }
+
+            return View(nguoiDung);
+        }
+
+        // POST: NguoiDungs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.NguoiDungs == null)
+            {
+                return Problem("Entity set 'DtsoftContext.NguoiDungs'  is null.");
+            }
+            var nguoiDung = await _context.NguoiDungs.FindAsync(id);
+            if (nguoiDung != null)
+            {
+                _context.NguoiDungs.Remove(nguoiDung);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NguoiDungExists(int id)
+        {
+          return (_context.NguoiDungs?.Any(e => e.IdNd == id)).GetValueOrDefault();
+        }
+    }
+}
