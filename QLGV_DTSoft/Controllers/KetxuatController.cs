d0bbba46cae224c9712587101bf038298c5c9506
Thai-devpro <65055494+Thai-devpro@users.noreply.github.com>
2023-06-27 10:27:01 +0700

    xóa thư viện dư

--- QLGV_DTSoft/Controllers/KetxuatController.cs
+++ QLGV_DTSoft/Controllers/KetxuatController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using DocumentFormat.OpenXml.InkML;
-using DocumentFormat.OpenXml.Vml.Spreadsheet;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
